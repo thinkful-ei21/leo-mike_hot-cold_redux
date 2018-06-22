d4a5e9ec85262af602e36a9a101e9cd7fdba131c
James Stuckey <jstuckey@rutgers.edu>
2018-06-22 14:45:50 -0400

    imported store to index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import './reset.css';
 import './index.css';
-
 import Game from './components/game';
+
+
+import store from './store';
 
 ReactDOM.render(
   <Game />,
